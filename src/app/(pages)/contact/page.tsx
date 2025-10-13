382bfd300e8c91bfa3ba8818de6eb4060cf1c206
unknown <kadlaon@uic.edu.ph>
2025-10-13 13:56:47 +0800

    Updated Contact Us page

--- src/app/(pages)/contact/page.tsx
+++ src/app/(pages)/contact/page.tsx
@@ -5,8 +5,22 @@ export default function ContactPage() {
     <main className="min-h-screen bg-rose-900 text-rose-50">
       <PageHero title="Contact Us" description="Reach out for inquiries and support." />
       <section className="mx-auto max-w-7xl px-6 py-10 space-y-2">
-        <p className="text-rose-100">Email: <a className="underline underline-offset-4" href="mailto:itconf@mail.com">itconf@mail.com</a></p>
-        <p className="text-rose-100">Phone: <a className="hover:text-rose-50" href="tel:+1800123456789">+1 800 123 456 789</a></p>
+        <p className="text-rose-100">Dr. Cherry Lyn Sta. Romana</p>
+        <p className="text-rose-100">CSP President</p>
+        <p className="text-rose-100">Email: <a className="underline underline-offset-4" href="mailto: cstaromana@gmail.com"> cstaromana@gmail.com</a></p>
+        <br></br>
+
+        <p className="text-rose-100">Dr. Judith J. Azcarraga</p>
+        <p className="text-rose-100">CSP Vice President</p>
+        <p className="text-rose-100">PCSC 2026 Conference Chair</p>
+        <p className="text-rose-100">Email: <a className="underline underline-offset-4" href="mailto: judith.azcarraga@dlsu.edu.ph"> judith.azcarraga@dlsu.edu.ph</a></p>
+        <br></br>
+
+        <p className="text-rose-100">Ms. Kristine Mae M. Adlaon</p>
+        <p className="text-rose-100">CSP Board Secretary</p>
+        <p className="text-rose-100">PCSC 2026 Local-Conference Chair</p>
+        <p className="text-rose-100">Email: <a className="underline underline-offset-4" href="mailto: kadlaon@uic.edu.ph"> kadlaon@uic.edu.ph</a></p>
+        <br></br>
       </section>
     </main>
   );
