e4d9878beaeaa47d4154bf5cfe94c31c7213be71
Al Gabriel <jaworski.orig@gmail.com>
2026-01-20 17:54:58 +0800

    fix: correct spelling of "Compiitition" to "Competition" in workshop title

--- src/app/(pages)/workshop/page.tsx
+++ src/app/(pages)/workshop/page.tsx
@@ -44,7 +44,7 @@ const workshops: Workshop[] = [
   },
   {
     id: 4,
-    title: "PCSC Programming Compiitition",
+    title: "PCSC Programming Competition",
     objective: "Programming Competition organized by the Computing Society of the Philippines – Special Interest Group for Women in Computing. The event will be held at the University of the Immaculate Conception, Davao City on April 23, 2026 from 1:30 – 3:30 PM. Each school may send at most 2 teams of 3 female students per team. Each team should be represented by aEach team should be represented by a coach.",
     organizers: [
       { name: "Computing Society of the Philippines – Special Interest Group for Women in Computing.", affiliation: "" }
